4c6316fe4f106a779e25b6605b3364ec86c7ba26
marianaarodrigues <marianaggl@gmail.com>
2023-05-23 15:19:59 -0300

    continuação

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@ function arearet() {
     let area = (num1 * num2).toFixed(2);
     document.getElementById('arearetang').innerHTML = area;
 }
+
+function areatri() {
+    let num1 = parseFloat(document.getElementById('basetriangulo').value);
+    let num2 = parseFloat(document.getElementById('alttriangulo').value);
+    let area = ((num1 * num2) / 2).toFixed(2);
+    document.getElementById('areatriang').innerHTML = area;
+}
+
+function hipoten() {
+    let num1 = parseFloat(document.getElementById('cateto1').value);
+    let num2 = parseFloat(document.getElementById('cateto2').value);
+    let hipotenusa = (Math.sqrt(num1 * num1 + num2 * num2)).toFixed(2);
+    document.getElementById('hipot').innerHTML = hipotenusa;
+}
+
+function bhaskar() {
+    let a = parseFloat(document.getElementById('valora').value);
+    let b = parseFloat(document.getElementById('valorb').value);
+    let c = parseFloat(document.getElementById('valorc').value);
+    let delta = parseFloat(b * b - 4 * a * c)
+    console.log(delta)
+    let raiz1 = parseFloat((-1 * b + Math.sqrt(delta)) / (2 * a))
+    let raiz2 = parseFloat((-1 * b - Math.sqrt(delta)) / (2 * a))
+    let resultado = raiz1 + ' e ' + raiz2
+    document.getElementById('bhask').innerHTML = resultado;
+}
