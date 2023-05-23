9471b06d72a6e6e41237f3910cd453708079bfd7
marianaarodrigues <marianaggl@gmail.com>
2023-05-23 12:23:37 -0300

    continuação

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-function soma2num() {
-    let num1 = parseFloat(document.getElementById('numero1').value);
-    let num2 = parseFloat(document.getElementById('numero2').value);
-    let soma = (num1 + num2).toFixed(2);
-    document.getElementById('soma').innerHTML = soma;
+function areacirc() {
+    let raio = parseFloat(document.getElementById('raio').value);
+    let area = (Math.PI * Math.pow(raio, 2)).toFixed(2);
+    document.getElementById('areacir').innerHTML = area;
+}
+
+function arearet() {
+    let num1 = parseFloat(document.getElementById('baseretangulo').value);
+    let num2 = parseFloat(document.getElementById('altretangulo').value);
+    let area = (num1 * num2).toFixed(2);
+    document.getElementById('arearetang').innerHTML = area;
 }
