5030de581a2eb599cfd0e4a6c41ee8f8243cb15e
marianaarodrigues <marianaggl@gmail.com>
2023-05-24 00:02:28 -0300

    continuação

--- js/script.js
+++ js/script.js
@@ -31,8 +31,8 @@ function bhaskar() {
     let c = parseFloat(document.getElementById('valorc').value)
     let delta = parseFloat(b * b - 4 * a * c)
     console.log(delta)
-    let raiz1 = parseFloat((-1 * b + Math.sqrt(delta)) / (2 * a))
-    let raiz2 = parseFloat((-1 * b - Math.sqrt(delta)) / (2 * a))
+    let raiz1 = (parseFloat((-1 * b + Math.sqrt(delta)) / (2 * a))).toFixed(2)
+    let raiz2 = (parseFloat((-1 * b - Math.sqrt(delta)) / (2 * a))).toFixed(2)
     let resultado = raiz1 + ' e ' + raiz2
     document.getElementById('bhask').innerHTML = resultado
 }
@@ -40,12 +40,26 @@ function bhaskar() {
 function realemdolar() {
     let cotacao = parseFloat(document.getElementById('cotacao').value)
     let real = parseFloat(document.getElementById('reais').value)
-    let dolar = real / cotacao
+    let dolar = (real / cotacao).toFixed(2)
     document.getElementById('realdolar').innerHTML = dolar
 }
 
 function celsfahr() {
     let celsius = parseFloat(document.getElementById('celsius').value)
-    let fahrenheit = 1.8 * celsius + 32
+    let fahrenheit = (1.8 * celsius + 32).toFixed(2)
     document.getElementById('celsiusfahr').innerHTML = fahrenheit
+}
+
+function msegundo() {
+    let velkm = parseFloat(document.getElementById('kmporhora').value)
+    let velm = (velkm / 3.6).toFixed(2)
+    document.getElementById('velmpors').innerHTML = velm
+}
+
+function media3notas() {
+    let n1 = parseFloat(document.getElementById('nota1').value)
+    let n2 = parseFloat(document.getElementById('nota2').value)
+    let n3 = parseFloat(document.getElementById('nota3').value)
+    let medianota = ((n1 + n2 + n3) / 3).toFixed(2)
+    document.getElementById('medianotas').innerHTML = medianota
 }
